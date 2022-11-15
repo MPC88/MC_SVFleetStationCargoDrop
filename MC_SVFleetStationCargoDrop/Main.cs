a13945e096fb5fa438d1c8098d5b71f045e1c6b6
MPC88 <myemail@gmail.com>
2022-11-15 16:48:26 +0000

    Added function and hotkey for unloading passengers.

--- MC_SVFleetStationCargoDrop/Main.cs
+++ MC_SVFleetStationCargoDrop/Main.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using System.Collections.Generic;
@@ -13,11 +14,78 @@ namespace MC_SVFleetStationCargoDrop
         public const string pluginName = "SV Fleet Station Cargo Drop";
         public const string pluginVersion = "1.0.0";
 
+        public static ConfigEntry<KeyCodeSubset> cfgModifier;
+        public static ConfigEntry<KeyCodeSubset> cfgCrewHotkey;
+
         private static List<int> droneEquipIDs;
 
         public void Awake()
         {
+            cfgCrewHotkey = Config.Bind<KeyCodeSubset>("Keybinds",
+                "Fleet disembark key",
+                KeyCodeSubset.X,
+                "Hotkey for disembark passengers from fleet ships.");
+            cfgModifier = Config.Bind<KeyCodeSubset>("Keybinds",
+                "Modifier key",
+                KeyCodeSubset.LeftAlt,
+                "Modifier key for fleet disembark.  Set to None for no modifer.");
             Harmony.CreateAndPatchAll(typeof(Main));
+        }
+
+        public void Update()
+        {
+            if(cfgModifier.Value == KeyCodeSubset.None || Input.GetKey((KeyCode)cfgModifier.Value))
+            {
+                if (Input.GetKeyDown((KeyCode)cfgCrewHotkey.Value))
+                    UnloadFleetPassengers();
+            }
+        }
+
+        private static void UnloadFleetPassengers()
+        {        
+            if (!(FleetControl.instance != null && FleetControl.instance.pc != null && PChar.Char.GetFleetSize > 0))
+            {
+                return;
+            }
+            if (!Inventory.instance.inStation)
+            {
+                InfoPanelControl.inst.ShowWarning(Lang.Get(0, 385), 4, false);
+                return;
+            }
+
+            Station currStation = Inventory.instance.currStation;
+            int num = 0;
+            Transform shipsTrans = AccessTools.FieldRefAccess<FleetControl, Transform>("shipsTrans")(FleetControl.instance);
+            CargoSystem playerCS = AccessTools.FieldRefAccess<FleetControl, CargoSystem>("playerCS")(FleetControl.instance);
+            if (shipsTrans == null || playerCS == null)
+                return;
+
+            for (int i = 0; i < shipsTrans.childCount; i++)
+            {
+                AIMercenaryCharacter aiMercChar = shipsTrans.GetChild(i).GetComponent<FleetMemberSlot>().aiMercChar;
+
+                if (aiMercChar.hangarDocked || aiMercChar.dockedStationID == currStation.id)
+                {
+                    List<CargoItem> cargo = aiMercChar.shipData.cargo;
+                    int num2 = 0;
+                    for (int j = 0; j < cargo.Count; j++)
+                    {
+                        CargoItem cargoItem = cargo[j];
+                        if(cargoItem.itemType == 5)
+                        {
+                            playerCS.StoreItem(cargoItem.itemType, cargoItem.itemID, cargoItem.rarity, cargoItem.qnt, cargoItem.pricePaid, cargoItem.sellerID, currStation.id, -1, cargoItem.extraData);
+                            num2 += cargoItem.qnt;
+                            cargo.RemoveAt(j);
+                            j--;
+                        }
+                    }
+                    if (num2 > 0)
+                    {
+                        SideInfo.AddMsg(aiMercChar.CommanderName(12) + " unloaded " + num2 + " passengers.");
+                    }
+                    num += num2;
+                }
+            }
         }
 
         [HarmonyPatch(typeof(FleetControl), nameof(FleetControl.UnloadFleetCargo))]
